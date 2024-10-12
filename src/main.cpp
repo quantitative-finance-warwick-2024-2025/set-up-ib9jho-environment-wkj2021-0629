b0ad284ac403d1cb42f0089c78a552115e7d30f7
Kejing Wang <wkj200109@163.com>
2024-10-12 12:04:39 +0100

    update

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,5 @@
 int main ()
 {
     // we want to print "My C Compiler is working!" instead
-    std::cout << "Change me before you submit the assignment!";
+    std::cout << "My C Compiler is working!";
 }
